d6cb9a9f3e52bb530650900073cc0a11c4eda9fd
Zhrey <zs1466439824@sina.com>
2018-07-23 22:47:26 +0800

    切换jetty服务

--- src/main/java/com/ray/ui/UiApplication.java
+++ src/main/java/com/ray/ui/UiApplication.java
@@ -1,10 +1,13 @@
 package com.ray.ui;
 
+import com.ray.cloud.framework.eureka.JettyFrameWorkServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
-public class UiApplication {
+public class UiApplication extends JettyFrameWorkServer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UiApplication.class, args);
