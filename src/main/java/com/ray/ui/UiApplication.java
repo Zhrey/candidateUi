c7ac70f7ad5c949bf001b2a77d4b064693e179b8
zhrey <zs1466439824@sina.com>
2018-07-26 18:13:31 +0800

    前端补充

--- src/main/java/com/ray/ui/UiApplication.java
+++ src/main/java/com/ray/ui/UiApplication.java
@@ -1,13 +1,20 @@
 package com.ray.ui;
 
 import com.ray.cloud.framework.eureka.JettyFrameWorkServer;
+import com.ray.core.sdk.service.CandidateManagementService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableDiscoveryClient
-@SpringBootApplication
-public class UiApplication extends JettyFrameWorkServer {
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
+@EnableFeignClients(basePackages = {"com.ray.core.sdk.service"})
+public class UiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UiApplication.class, args);
--- src/main/java/com/ray/ui/controller/LoginController.java
+++ src/main/java/com/ray/ui/controller/LoginController.java
@@ -1,5 +1,7 @@
 package com.ray.ui.controller;
 
+import com.ray.core.sdk.service.CandidateManagementService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,18 +14,22 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 @Controller
 @RequestMapping("")
-public class HomeController {
+public class LoginController {
 
     private static final String LOGIN_PAGE = "login";
     private static final String HOME_PAGE = "home";
 
+    @Autowired
+    private CandidateManagementService candidateManagementService;
+
     @RequestMapping("")
-    public String login() {
+    public String toLoginPage() {
         return LOGIN_PAGE;
     }
     @RequestMapping("/login")
     @ResponseBody
-    public String toHomePage() {
+    public String login() {
+        candidateManagementService.toHomePage();
         return HOME_PAGE;
     }
 }
