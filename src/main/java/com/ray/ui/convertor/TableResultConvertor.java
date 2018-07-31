c0b78450e624a326731ecff58c23c8b4c2caf5e2
zhrey <zs1466439824@sina.com>
2018-07-31 17:33:45 +0800

    遗漏

---
+++ src/main/java/com/ray/ui/convertor/TableResultConvertor.java
@@ -0,0 +1,12 @@
+package com.ray.ui.convertor;
+
+/**
+ * @Author: ZhangRui
+ * @Description:
+ * @date: Created in 17:22 2018/7/31
+ * @Modified By:
+ */
+public class TableResultConvertor {
+
+
+}
